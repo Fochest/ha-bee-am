f8f7306ecd3efe28c5e8d731dd07721819900572
Fochest <fochest@gmail.com>
2025-09-19 16:39:41 +0200

    added state_class attribute to sensors sensor.py so that HomeAssistant recognizes them as Energy-attributes

--- custom_components/beaam/sensor.py
+++ custom_components/beaam/sensor.py
@@ -1,4 +1,7 @@
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorStateClass,
+)
 from homeassistant.const import (
     UnitOfPower,
     UnitOfEnergy,
@@ -8,19 +11,18 @@ from .const import DOMAIN
 
 # Mapping von Beaam-Keys auf Einheit & Device Class
 SENSOR_DEFINITIONS = {
-    "POWER_PRODUCTION": (UnitOfPower.WATT, "power"),
-    "POWER_CONSUMPTION_CALC": (UnitOfPower.WATT, "power"),
-    "POWER_GRID": (UnitOfPower.WATT, "power"),
-    "POWER_STORAGE": (UnitOfPower.WATT, "power"),
-    "ENERGY_PRODUCED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_CONSUMED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_IMPORTED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_EXPORTED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_CHARGED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_DISCHARGED": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "ENERGY_CONSUMED_CALC": (UnitOfEnergy.WATT_HOUR, "energy"),
-    "STATE_OF_CHARGE": (PERCENTAGE, "battery"),
-    "SELF_SUFFICIENCY": (PERCENTAGE, None),
+    "POWER_PRODUCTION": (UnitOfPower.WATT, "power", SensorStateClass.MEASUREMENT),
+    "POWER_CONSUMPTION_CALC": (UnitOfPower.WATT, "power", SensorStateClass.MEASUREMENT),
+    "POWER_GRID": (UnitOfPower.WATT, "power", SensorStateClass.MEASUREMENT),
+    "POWER_STORAGE": (UnitOfPower.WATT, "power", SensorStateClass.MEASUREMENT),
+    "ENERGY_PRODUCED": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "ENERGY_CONSUMED_CALC": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "ENERGY_IMPORTED": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "ENERGY_EXPORTED": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "ENERGY_CHARGED": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "ENERGY_DISCHARGED": (UnitOfEnergy.WATT_HOUR, "energy", SensorStateClass.TOTAL_INCREASING),
+    "STATE_OF_CHARGE": (PERCENTAGE, "battery", SensorStateClass.MEASUREMENT),
+    "SELF_SUFFICIENCY": (PERCENTAGE, None, SensorStateClass.MEASUREMENT)
     # Fraktionen → werden zusätzlich dynamisch behandelt
 }
 
@@ -41,13 +43,13 @@ class BeaamSensor(SensorEntity):
     def __init__(self, coordinator, key):
         self.coordinator = coordinator
         self._key = key
-        definition = SENSOR_DEFINITIONS.get(key, (None, None))
-        self._unit, self._device_class = definition
+        definition = SENSOR_DEFINITIONS.get(key, (None, None, None))
+        self._unit, self._device_class, self._state_class = definition
 
-        # Fraktionswerte → immer in Prozent
         if key.startswith("FRACTION_"):
             self._unit = PERCENTAGE
             self._device_class = None
+            self._state_class = SensorStateClass.MEASUREMENT
 
     @property
     def name(self):
@@ -56,6 +58,10 @@ class BeaamSensor(SensorEntity):
     @property
     def unique_id(self):
         return f"beaam_{self._key.lower()}"
+
+    @property
+    def state_class(self):
+        return self._state_class
 
     @property
     def native_value(self):
